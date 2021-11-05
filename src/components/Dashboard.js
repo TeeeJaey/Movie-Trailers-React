078f3ecec74abf53da2188ce067c068006ce72bf
Tejas Jadhav <tjadhav95@gmail.com>
2021-11-05 23:43:23 +0530

    movie-block details and fixed header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,16 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import '../Styles/dashboard.css';
 import AppliedFilters from "./AppliedFilters";
-import startBtnImage from "../images/start-btn.png";
+import startBtnImage from "../Images/start-btn.png";
 import Constants from "../Utils/Constants";
+import { MdThumbUp } from "react-icons/md";
 
 export default function Dashboard() {
     const dispatch = useDispatch();
     let moviesData = useSelector(state => state.moviesData);
     let sortBy = useSelector(state => state.sortBy);
+    let languageFilter = useSelector(state => state.languageFilter);
+    let genreFilter = useSelector(state => state.genreFilter);
     
     let movieList = [];
     for(const key in moviesData) {
@@ -16,13 +19,19 @@ export default function Dashboard() {
         movie.key = key;
         movieList.push(movie);
     }
+
+    if(languageFilter && languageFilter.length > 0)
+        movieList = movieList.filter(x=>languageFilter.includes(x.EventLanguage));
+    
     
     if(sortBy == Constants.SortBy.Fresh) {
-        movieList.sort((a,b) => a.trailerUploadDate - b.trailerUploadDate);
+        movieList.sort((a,b) => b.trailerUploadDate - a.trailerUploadDate);
     }
     else if(sortBy == Constants.SortBy.Popular) {
-        movieList.sort((a,b) => a.wtsCount - b.wtsCount);
+        movieList.sort((a,b) => b.wtsCount - a.wtsCount);
     }
+
+    const months = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
     return (
         <div className="dashboard">
@@ -30,17 +39,53 @@ export default function Dashboard() {
 
             <div className="movie-list">
                 {movieList.map((movie)=>{
-                    return  <div key={movie.key} className="movie">
-                                <div className="movie-image">
-                                    <img src={movie.EventImageUrl} 
-                                         className="image"
-                                         alt={movie.EventTitle} />
-                                    <img src={startBtnImage} className="start-btn" />
-                                </div>
-                                <div className="movie-name">
-                                    {movie.EventTitle}
-                                </div>
-                            </div>;
+
+                    let genreSatisfied = false;
+                    if(genreFilter && genreFilter.length > 0) {
+                        let genreList = movie.EventGenre.split('|');
+                        genreList.forEach(genre => {
+                            if(genreFilter.includes(genre)) {
+                                genreSatisfied = true;
+                            }
+                        }); 
+                    }
+                    else 
+                        genreSatisfied = true;
+                    
+                    if(genreSatisfied) {
+                        const date = movie.TrailerURLUploadDate.split('-');
+                        const year = parseInt(date[0]);
+                        const month = months[parseInt(date[1])];
+
+                        return  <div key={movie.key} className="movie">
+                                    <div className="movie-image">
+                                        <div className="movie-date">
+                                            <span className="month">{month}</span>
+                                            <span className="year">{year}</span>
+                                        </div>
+
+                                        <img src={movie.EventImageUrl} 
+                                            className="image"
+                                            alt={movie.EventTitle} />
+                                        <img src={startBtnImage} className="start-btn" />
+                                        
+                                        <div className="movie-rating">
+                                            <div className="flex-evenly">
+                                                <MdThumbUp className="thumbsup-ico"/>
+                                                <div>{movie.wtsPerc}%</div>
+                                            </div>
+                                            <div className="flex-evenly">
+                                                <div className="vote-count">{movie.wtsCount} votes</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="movie-name">
+                                        {movie.EventTitle}
+                                    </div>
+                                </div>;
+                    }
+                    else
+                        return <></>;
                 })}
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './Styles/styles.css';
+import './Styles/index.css';
 
 import Main from './Components/Main.js';
 import store from './Store/Store.js';
